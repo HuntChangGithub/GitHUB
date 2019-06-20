9d8f82977e6ee33ce32b82c1467a183e14850b84
HuntChangGithub <525022859@qq.com>
2019-06-20 11:19:39 +0800

    my git

--- java/Hello.java
+++ java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
   public static void main(String[] args){
-    System.out.println("this is my first github file...")
+    System.out.println("this is my first github file...");
+	System.out.println("บรเห฿บ...");
   }
 }
